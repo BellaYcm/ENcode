e333b5f1fb87a24d06cc633855b5205ebb7aad9b
sunkejl <sunkejl@live.com>
2014-12-10 16:30:10 +0800

    refs 5

---
+++ Encode.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator_sk
+ * Date: 2014/12/10
+ * Time: 15:39
+ */
+class Encode{
+    /**
+     * @param $code
+     * @param string $message
+     * @param array $data
+     */
+    //json 不支持GBK
+    public static  function json($code,$message = "",$data=array()){
+        if(!is_numeric($code)){
+            return "";
+        }
+        $result=array(
+            'code'=>$code,
+            'message'=>$message,
+            'data'=>$data
+        );
+        echo json_encode($result);
+        exit;
+    }
+    //\n换行
+    public static  function xml(){
+        header("Content-Type:text/xml");
+        $xml="<?xml version='1.0' encoding='UTF-8'?>\n";
+        $xml.="<root>\n";
+        $xml.="<code>200</code>";
+        $xml.="<message>message</message>";
+        $xml.="</root>";
+        echo $xml;
+    }
+    public static function xmlEncode($code,$message,$data=array()){
+        if(!is_numeric($code)){
+            return"";
+        }
+        $result=array(
+            'code'=>$code,
+            'message'=>$message,
+            'data'=>$data,
+        );
+        header("Content-Type:text/xml");
+        $xml="<?xml version='1.0' encoding='UTF-8'?>";
+        $xml.="<root>";
+        $xml.=self::xmlToEncode($result);
+        $xml.="</root>";
+        echo $xml;
+
+    }
+    public static function xmlToEncode($data){
+        $xml="";
+        foreach($data as $key =>$value){
+            $xml.="<{$key}>";#{}当成变量
+            $xml.=$value;
+            $xml.="</{$key}>";
+        }
+        return $xml;
+
+    }
+
+}
